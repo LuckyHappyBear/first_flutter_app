0ad71ade93e0e5bec979cdd20cfac715c68e35cb
fosterhe <admin@Fosterhe-MacStudio.local>
2024-05-23 15:03:09 +0800

    feat: 进度指示器示例

--- lib/main.dart
+++ lib/main.dart
@@ -135,6 +135,25 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
+            // 线性进度条高度指定为3
+            SizedBox(
+              height: 3,
+              child: LinearProgressIndicator(
+                backgroundColor: Colors.grey[200],
+                valueColor: const AlwaysStoppedAnimation(Colors.blue),
+                value: .5,
+              ),
+            ),
+// 圆形进度条直径指定为100
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.grey[200],
+                valueColor: const AlwaysStoppedAnimation(Colors.blue),
+                value: .7,
+              ),
+            ),
           ],
         ),
       ),
