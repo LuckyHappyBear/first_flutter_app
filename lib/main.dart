feb20d109eb6255ab163840bc75e59156d629fb0
fosterhe <admin@Fosterhe-MacStudio.local>
2024-05-23 14:19:56 +0800

    feat: 单选开关&复选框示例

--- lib/main.dart
+++ lib/main.dart
@@ -119,7 +119,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   return const ImageAndIconRoute();
                 }));
               },
-            )
+            ),
+            const SwitchAndCheckBoxTestRoute(),
           ],
         ),
       ),
@@ -213,6 +214,43 @@ class ImageAndIconRoute extends StatelessWidget {
           ],
         );
       }).toList()),
+    );
+  }
+}
+
+class SwitchAndCheckBoxTestRoute extends StatefulWidget {
+  const SwitchAndCheckBoxTestRoute({super.key});
+
+  @override
+  State<SwitchAndCheckBoxTestRoute> createState() => _SwitchAndCheckBoxTestRouteState();
+}
+
+class _SwitchAndCheckBoxTestRouteState extends State<SwitchAndCheckBoxTestRoute> {
+  bool _switchSelected = true; //维护单选开关状态
+  bool _checkboxSelected = true; //维护复选框状态
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Switch(
+          value: _switchSelected, //当前状态
+          onChanged: (value) {
+            //重新构建页面
+            setState(() {
+              _switchSelected = value;
+            });
+          },
+        ),
+        Checkbox(
+          value: _checkboxSelected,
+          activeColor: Colors.red, //选中时的颜色
+          onChanged: (value) {
+            setState(() {
+              _checkboxSelected = value ?? false;
+            });
+          },
+        )
+      ],
     );
   }
 }
