06461b7d0aa34ef82bef3a20ebd709ac594309ff
fosterhe <admin@Fosterhe-MacStudio.local>
2024-05-23 14:58:05 +0800

    feat: 文本框简单示例

--- lib/main.dart
+++ lib/main.dart
@@ -121,6 +121,20 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             const SwitchAndCheckBoxTestRoute(),
+            const Column(
+              children: <Widget>[
+                TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      labelText: "用户名", hintText: "用户名或邮箱", prefixIcon: Icon(Icons.person)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "密码", hintText: "您的登录密码", prefixIcon: Icon(Icons.lock)),
+                  obscureText: true,
+                ),
+              ],
+            ),
           ],
         ),
       ),
