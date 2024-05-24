ae044059725a4d09e2609320059b68484e281d57
fosterhe <admin@Fosterhe-MacStudio.local>
2024-05-24 18:16:34 +0800

    动画切换组件示例

--- lib/main.dart
+++ lib/main.dart
@@ -110,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) {
-                    return const StaggerRoute();
+                    return const AnimatedSwitcherCounterRoute();
                   }),
                 );
               },
@@ -128,134 +128,45 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class StaggerAnimation extends StatelessWidget {
-  StaggerAnimation({
-    Key? key,
-    required this.controller,
-  }) : super(key: key) {
-    //高度动画
-    height = Tween<double>(
-      begin: .0,
-      end: 300.0,
-    ).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: const Interval(
-          0.0, 0.6, //间隔，前60%的动画时间
-          curve: Curves.ease,
-        ),
-      ),
-    );
-
-    color = ColorTween(
-      begin: Colors.green,
-      end: Colors.red,
-    ).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: const Interval(
-          0.0, 0.6, //间隔，前60%的动画时间
-          curve: Curves.ease,
-        ),
-      ),
-    );
-
-    padding = Tween<EdgeInsets>(
-      begin: const EdgeInsets.only(left: .0),
-      end: const EdgeInsets.only(left: 100.0),
-    ).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: const Interval(
-          0.6, 1.0, //间隔，后40%的动画时间
-          curve: Curves.ease,
-        ),
-      ),
-    );
-  }
-
-  late final Animation<double> controller;
-  late final Animation<double> height;
-  late final Animation<EdgeInsets> padding;
-  late final Animation<Color?> color;
-
-  Widget _buildAnimation(BuildContext context, child) {
-    return Container(
-      alignment: Alignment.bottomCenter,
-      padding: padding.value,
-      child: Container(
-        color: color.value,
-        width: 50.0,
-        height: height.value,
-      ),
-    );
-  }
+class AnimatedSwitcherCounterRoute extends StatefulWidget {
+  const AnimatedSwitcherCounterRoute({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      builder: _buildAnimation,
-      animation: controller,
-    );
-  }
+  State<AnimatedSwitcherCounterRoute> createState() => _AnimatedSwitcherCounterRouteState();
 }
 
-class StaggerRoute extends StatefulWidget {
-  const StaggerRoute({super.key});
-
-  @override
-  State<StaggerRoute> createState() => _StaggerRouteState();
-}
-
-class _StaggerRouteState extends State<StaggerRoute> with TickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 2000),
-      vsync: this,
-    );
-  }
-
-  _playAnimation() async {
-    try {
-      //先正向执行动画
-      await _controller.forward().orCancel;
-      //再反向执行动画
-      await _controller.reverse().orCancel;
-    } on TickerCanceled {
-      //捕获异常。可能发生在组件销毁时，计时器会被取消。
-    }
-  }
+class _AnimatedSwitcherCounterRouteState extends State<AnimatedSwitcherCounterRoute> {
+  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("交织动画"),
-      ),
       body: Center(
         child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () => _playAnimation(),
-              child: const Text("start animation"),
-            ),
-            Container(
-              width: 300.0,
-              height: 300.0,
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.1),
-                border: Border.all(
-                  color: Colors.black.withOpacity(0.5),
-                ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                //执行缩放动画
+                return ScaleTransition(scale: animation, child: child);
+              },
+              child: Text(
+                '$_count',
+                //显示指定key，不同的key会被认为是不同的Text，这样才能执行动画
+                key: ValueKey<int>(_count),
+                style: Theme.of(context).textTheme.headline4,
               ),
-              //调用我们定义的交错动画Widget
-              child: StaggerAnimation(controller: _controller),
+            ),
+            ElevatedButton(
+              child: const Text(
+                '+1',
+              ),
+              onPressed: () {
+                setState(() {
+                  _count += 1;
+                });
+              },
             ),
           ],
         ),
