ae18870bbe5bbe8e52f9e8fc5d8e96d3f9efed7d
fosterhe <admin@Fosterhe-MacStudio.local>
2024-05-24 10:26:25 +0800

    feat: NotificationListener滚动监听示例

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) {
-                  return const ScrollControllerTestRoute();
+                  return const ScrollNotificationTestRoute();
                 }),
               );
             },
@@ -111,69 +111,58 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class ScrollControllerTestRoute extends StatefulWidget {
-
-  const ScrollControllerTestRoute({super.key});
+class ScrollNotificationTestRoute extends StatefulWidget {
+  const ScrollNotificationTestRoute({super.key});
 
   @override
-  ScrollControllerTestRouteState createState() {
-    return ScrollControllerTestRouteState();
-  }
+  State<ScrollNotificationTestRoute> createState() =>
+      _ScrollNotificationTestRouteState();
 }
 
-class ScrollControllerTestRouteState extends State<ScrollControllerTestRoute> {
-  final ScrollController _controller = ScrollController();
-  bool showToTopBtn = false; //是否显示“返回到顶部”按钮
-
-  @override
-  void initState() {
-    super.initState();
-    //监听滚动事件，打印滚动位置
-    _controller.addListener(() {
-      print(_controller.offset); //打印滚动位置
-      if (_controller.offset < 1000 && showToTopBtn) {
-        setState(() {
-          showToTopBtn = false;
-        });
-      } else if (_controller.offset >= 1000 && showToTopBtn == false) {
-        setState(() {
-          showToTopBtn = true;
-        });
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    //为了避免内存泄露，需要调用_controller.dispose
-    _controller.dispose();
-    super.dispose();
-  }
+class _ScrollNotificationTestRouteState
+    extends State<ScrollNotificationTestRoute> {
+  String _progress = "0%"; //保存进度百分比
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("滚动控制")),
-      body: Scrollbar(
-        child: ListView.builder(
-            itemCount: 100,
-            itemExtent: 50.0, //列表项高度固定时，显式指定高度是一个好习惯(性能消耗小)
-            controller: _controller,
-            itemBuilder: (context, index) {
-              return ListTile(title: Text("$index"),);
-            }
-        ),
+      appBar: AppBar(
+        title: const Text("滚动通知监听"),
       ),
-      floatingActionButton: !showToTopBtn ? null : FloatingActionButton(
-          child: const Icon(Icons.arrow_upward),
-          onPressed: () {
-            //返回到顶部时执行动画
-            _controller.animateTo(
-              .0,
-              duration: const Duration(milliseconds: 200),
-              curve: Curves.ease,
-            );
-          }
+      body: Center(
+        child:  Scrollbar(
+          //进度条
+          // 监听滚动通知
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification notification) {
+              double progress = notification.metrics.pixels /
+                  notification.metrics.maxScrollExtent;
+              //重新构建
+              setState(() {
+                _progress = "${(progress * 100).toInt()}%";
+              });
+              print("BottomEdge: ${notification.metrics.extentAfter == 0}");
+              return false;
+              //return true; //放开此行注释后，进度条将失效
+            },
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                ListView.builder(
+                  itemCount: 100,
+                  itemExtent: 50.0,
+                  itemBuilder: (context, index) => ListTile(title: Text("$index")),
+                ),
+                CircleAvatar(
+                  //显示进度百分比
+                  radius: 30.0,
+                  backgroundColor: Colors.black54,
+                  child: Text(_progress),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
