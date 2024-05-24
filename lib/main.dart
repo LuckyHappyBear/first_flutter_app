da6bc255d247f83a8c67b4df6838967b14feeffd
fosterhe <admin@Fosterhe-MacStudio.local>
2024-05-24 11:40:34 +0800

    feat: GridView简单示例

--- lib/main.dart
+++ lib/main.dart
@@ -88,82 +88,26 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(children: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) {
-                  return const ScrollNotificationTestRoute();
-                }),
-              );
-            },
-            child: const Text("open new route"),
-          )
-        ]),
+        child: GridView(
+          padding: EdgeInsets.zero,
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 120.0, childAspectRatio: 2.0 //宽高比为2
+              ),
+          children: const <Widget>[
+            Icon(Icons.ac_unit),
+            Icon(Icons.airport_shuttle),
+            Icon(Icons.all_inclusive),
+            Icon(Icons.beach_access),
+            Icon(Icons.cake),
+            Icon(Icons.free_breakfast),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class ScrollNotificationTestRoute extends StatefulWidget {
-  const ScrollNotificationTestRoute({super.key});
-
-  @override
-  State<ScrollNotificationTestRoute> createState() =>
-      _ScrollNotificationTestRouteState();
-}
-
-class _ScrollNotificationTestRouteState
-    extends State<ScrollNotificationTestRoute> {
-  String _progress = "0%"; //保存进度百分比
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("滚动通知监听"),
-      ),
-      body: Center(
-        child:  Scrollbar(
-          //进度条
-          // 监听滚动通知
-          child: NotificationListener<ScrollNotification>(
-            onNotification: (ScrollNotification notification) {
-              double progress = notification.metrics.pixels /
-                  notification.metrics.maxScrollExtent;
-              //重新构建
-              setState(() {
-                _progress = "${(progress * 100).toInt()}%";
-              });
-              print("BottomEdge: ${notification.metrics.extentAfter == 0}");
-              return false;
-              //return true; //放开此行注释后，进度条将失效
-            },
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                ListView.builder(
-                  itemCount: 100,
-                  itemExtent: 50.0,
-                  itemBuilder: (context, index) => ListTile(title: Text("$index")),
-                ),
-                CircleAvatar(
-                  //显示进度百分比
-                  radius: 30.0,
-                  backgroundColor: Colors.black54,
-                  child: Text(_progress),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
