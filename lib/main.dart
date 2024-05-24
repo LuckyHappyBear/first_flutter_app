eae9e2151f172bf3a095ee0b9c6d6160744e5630
fosterhe <admin@Fosterhe-MacStudio.local>
2024-05-24 17:45:25 +0800

    动画示例

--- lib/main.dart
+++ lib/main.dart
@@ -105,13 +105,17 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return const ScaleAnimationRoute();
+                  }),
+                );
+              },
+              child: const Text("open animation route"),
+            )
           ],
         ),
       ),
@@ -121,5 +125,68 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class ScaleAnimationRoute extends StatefulWidget {
+  const ScaleAnimationRoute({Key? key}) : super(key: key);
+
+  @override
+  State<ScaleAnimationRoute> createState() => _ScaleAnimationRouteState();
+}
+
+// 需要继承TickerProvider，如果有多个AnimationController，则应该使用TickerProviderStateMixin。
+class _ScaleAnimationRouteState extends State<ScaleAnimationRoute>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    //匀速
+    //图片宽高从0变到300
+    animation = Tween(begin: 0.0, end: 300.0).animate(controller)
+      ..addListener(() {
+        setState(() => {});
+      });
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        //动画执行结束时反向执行动画
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        //动画恢复到初始状态时执行动画（正向）
+        controller.forward();
+      }
+    });
+
+    //启动动画(正向执行)
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.network(
+          "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+          width: animation.value,
+          height: animation.value,
+        ),
+      ),
+    );
+  }
+
+  @override
+  dispose() {
+    //路由销毁时需要释放动画资源
+    controller.dispose();
+    super.dispose();
   }
 }
