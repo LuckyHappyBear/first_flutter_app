9ddf188cd953f4183c58b1c42fe8f978792342b0
fosterhe <admin@Fosterhe-MacStudio.local>
2024-05-24 18:28:41 +0800

    动画过渡组件示例

--- lib/main.dart
+++ lib/main.dart
@@ -110,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) {
-                    return const AnimatedSwitcherCounterRoute();
+                    return const AnimatedWidgetsTest();
                   }),
                 );
               },
@@ -128,47 +128,131 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class AnimatedSwitcherCounterRoute extends StatefulWidget {
-  const AnimatedSwitcherCounterRoute({super.key});
+class AnimatedWidgetsTest extends StatefulWidget {
+  const AnimatedWidgetsTest({Key? key}) : super(key: key);
 
   @override
-  State<AnimatedSwitcherCounterRoute> createState() => _AnimatedSwitcherCounterRouteState();
+  State<AnimatedWidgetsTest> createState() => _AnimatedWidgetsTestState();
 }
 
-class _AnimatedSwitcherCounterRouteState extends State<AnimatedSwitcherCounterRoute> {
-  int _count = 0;
+class _AnimatedWidgetsTestState extends State<AnimatedWidgetsTest> {
+  double _padding = 10;
+  var _align = Alignment.topRight;
+  double _height = 100;
+  double _left = 0;
+  Color _color = Colors.red;
+  TextStyle _style = const TextStyle(color: Colors.black);
+  double _opacity = 1;
 
   @override
   Widget build(BuildContext context) {
+    var duration = const Duration(milliseconds: 400);
     return Scaffold(
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            AnimatedSwitcher(
-              duration: const Duration(milliseconds: 500),
-              transitionBuilder: (Widget child, Animation<double> animation) {
-                //执行缩放动画
-                return ScaleTransition(scale: animation, child: child);
-              },
-              child: Text(
-                '$_count',
-                //显示指定key，不同的key会被认为是不同的Text，这样才能执行动画
-                key: ValueKey<int>(_count),
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
             ElevatedButton(
-              child: const Text(
-                '+1',
-              ),
               onPressed: () {
                 setState(() {
-                  _count += 1;
+                  _padding = 20;
                 });
               },
+              child: AnimatedPadding(
+                duration: duration,
+                padding: EdgeInsets.all(_padding),
+                child: const Text("AnimatedPadding"),
+              ),
             ),
-          ],
+            SizedBox(
+              height: 50,
+              child: Stack(
+                children: <Widget>[
+                  AnimatedPositioned(
+                    duration: duration,
+                    left: _left,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _left = 100;
+                        });
+                      },
+                      child: const Text("AnimatedPositioned"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 100,
+              color: Colors.grey,
+              child: AnimatedAlign(
+                duration: duration,
+                alignment: _align,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _align = Alignment.center;
+                    });
+                  },
+                  child: const Text("AnimatedAlign"),
+                ),
+              ),
+            ),
+            AnimatedContainer(
+              duration: duration,
+              height: _height,
+              color: _color,
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    _height = 150;
+                    _color = Colors.blue;
+                  });
+                },
+                child: const Text(
+                  "AnimatedContainer",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            AnimatedDefaultTextStyle(
+              style: _style,
+              duration: duration,
+              child: GestureDetector(
+                child: const Text("hello world"),
+                onTap: () {
+                  setState(() {
+                    _style = const TextStyle(
+                      color: Colors.blue,
+                      decorationStyle: TextDecorationStyle.solid,
+                      decorationColor: Colors.blue,
+                    );
+                  });
+                },
+              ),
+            ),
+            AnimatedOpacity(
+              opacity: _opacity,
+              duration: duration,
+              child: TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                onPressed: () {
+                  setState(() {
+                    _opacity = 0.2;
+                  });
+                },
+                child: const Text(
+                  "AnimatedOpacity",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ].map((e) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: e,
+            );
+          }).toList(),
         ),
       ),
     );
